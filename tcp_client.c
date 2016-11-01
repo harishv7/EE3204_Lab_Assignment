4da4a3c4e58a15f9f3b01f38ac66ddcfd85acb7c
Harish V <harishv207@gmail.com>
2016-11-01 23:50:19 +0800

    Formatted base code

--- tcp_client.c
+++ tcp_client.c
@@ -4,11 +4,13 @@ tcp_client.c: the source file of the client in tcp transmission
 
 #include "headsock.h"
 
-float str_cli(FILE *fp, int sockfd, long *len);                       //transmission function
-void tv_sub(struct  timeval *out, struct timeval *in);	    //calcu the time interval between out and in
+//transmission function
+float str_cli(FILE *fp, int sockfd, long *len);
 
-int main(int argc, char **argv)
-{
+// calculate the time interval between out and in
+void tv_sub(struct  timeval *out, struct timeval *in);	    
+
+int main(int argc, char **argv) {
 	int sockfd, ret;
 	float ti, rt;
 	long len;
@@ -22,17 +24,21 @@ int main(int argc, char **argv)
 		printf("parameters not match");
 	}
 
-	sh = gethostbyname(argv[1]);	                                       //get host's information
+	// get host's information
+	sh = gethostbyname(argv[1]);
 	if (sh == NULL) {
 		printf("error when gethostby name");
 		exit(0);
 	}
 
-	printf("canonical name: %s\n", sh->h_name);					//print the remote host's information
-	for (pptr=sh->h_aliases; *pptr != NULL; pptr++)
+	// print the remote host's information
+	printf("canonical name: %s\n", sh->h_name);
+
+	for (pptr=sh->h_aliases; *pptr != NULL; pptr++) {
 		printf("the aliases name is: %s\n", *pptr);
-	switch(sh->h_addrtype)
-	{
+	}
+
+	switch(sh->h_addrtype) {
 		case AF_INET:
 			printf("AF_INET\n");
 		break;
@@ -42,41 +48,51 @@ int main(int argc, char **argv)
 	}
         
 	addrs = (struct in_addr **)sh->h_addr_list;
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);                           //create the socket
-	if (sockfd <0)
-	{
+
+	// create the socket
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (sockfd <0) {
 		printf("error in socket");
 		exit(1);
 	}
+
 	ser_addr.sin_family = AF_INET;                                                      
 	ser_addr.sin_port = htons(MYTCP_PORT);
+
 	memcpy(&(ser_addr.sin_addr.s_addr), *addrs, sizeof(struct in_addr));
+
 	bzero(&(ser_addr.sin_zero), 8);
-	ret = connect(sockfd, (struct sockaddr *)&ser_addr, sizeof(struct sockaddr));         //connect the socket with the host
+
+	// connect the socket with the host
+	ret = connect(sockfd, (struct sockaddr *)&ser_addr, sizeof(struct sockaddr));
+
 	if (ret != 0) {
 		printf ("connection failed\n"); 
 		close(sockfd); 
 		exit(1);
 	}
 	
-	if((fp = fopen ("myfile.txt","r+t")) == NULL)
-	{
+	if((fp = fopen ("myfile.txt","r+t")) == NULL) {
 		printf("File doesn't exit\n");
 		exit(0);
 	}
 
-	ti = str_cli(fp, sockfd, &len);                       //perform the transmission and receiving
-	rt = (len/(float)ti);                                         //caculate the average transmission rate
+	// perform the transmission and receiving
+	ti = str_cli(fp, sockfd, &len);
+
+	//caculate the average transmission rate
+	rt = (len/(float)ti);
+
 	printf("Time(ms) : %.3f, Data sent(byte): %d\nData rate: %f (Kbytes/s)\n", ti, (int)len, rt);
 
 	close(sockfd);
 	fclose(fp);
-//}
+
 	exit(0);
 }
 
-float str_cli(FILE *fp, int sockfd, long *len)
-{
+float str_cli(FILE *fp, int sockfd, long *len) {
 	char *buf;
 	long lsize, ci;
 	char sends[DATALEN];
@@ -92,50 +108,67 @@ float str_cli(FILE *fp, int sockfd, long *len)
 	printf("The file length is %d bytes\n", (int)lsize);
 	printf("the packet length is %d bytes\n",DATALEN);
 
-// allocate memory to contain the whole file.
+	// allocate memory to contain the whole file.
 	buf = (char *) malloc (lsize);
 	if (buf == NULL) exit (2);
 
-  // copy the file into the buffer.
+  	// copy the file into the buffer.
 	fread (buf,1,lsize,fp);
 
-  /*** the whole file is loaded in the buffer. ***/
-	buf[lsize] ='\0';									//append the end byte
-	gettimeofday(&sendt, NULL);							//get the current time
-	while(ci<= lsize)
-	{
-		if ((lsize+1-ci) <= DATALEN)
+  	/*** the whole file is loaded in the buffer. ***/
+  	// append the end byte (null byte to terminate string)
+	buf[lsize] ='\0';
+
+	// get the current time
+	gettimeofday(&sendt, NULL);
+
+	while(ci<= lsize) {
+		if ((lsize+1-ci) <= DATALEN) {
 			slen = lsize+1-ci;
-		else 
+		} else {
 			slen = DATALEN;
+		}
+
 		memcpy(sends, (buf+ci), slen);
+
+		// send the data
 		n = send(sockfd, &sends, slen, 0);
+
 		if(n == -1) {
-			printf("send error!");								//send the data
+			printf("send error!");
 			exit(1);
 		}
 		ci += slen;
 	}
-	if ((n= recv(sockfd, &ack, 2, 0))==-1)                                   //receive the ack
-	{
+
+	// receive the ack
+	if ((n= recv(sockfd, &ack, 2, 0))==-1) {
 		printf("error when receiving\n");
 		exit(1);
 	}
-	if (ack.num != 1|| ack.len != 0)
+
+	if (ack.num != 1|| ack.len != 0) {
 		printf("error in transmission\n");
+	}
+
+	// get current time
 	gettimeofday(&recvt, NULL);
-	*len= ci;                                                         //get current time
-	tv_sub(&recvt, &sendt);                                                                 // get the whole trans time
+
+	*len= ci;
+
+	// get the whole trans time
+	tv_sub(&recvt, &sendt);
+
 	time_inv += (recvt.tv_sec)*1000.0 + (recvt.tv_usec)/1000.0;
+
 	return(time_inv);
 }
 
-void tv_sub(struct  timeval *out, struct timeval *in)
-{
-	if ((out->tv_usec -= in->tv_usec) <0)
-	{
+void tv_sub(struct  timeval *out, struct timeval *in) {
+	if ((out->tv_usec -= in->tv_usec) <0) {
 		--out ->tv_sec;
 		out ->tv_usec += 1000000;
 	}
+	
 	out->tv_sec -= in->tv_sec;
 }
--- tcp_server.c
+++ tcp_server.c
@@ -7,68 +7,76 @@ tcp_ser.c: the source file of the server in tcp transmission
 
 #define BACKLOG 10
 
-void str_ser(int sockfd);                                                        // transmitting and receiving function
+// transmitting and receiving function
+void str_ser(int sockfd);
 
-int main(void)
-{
+int main(void) {
 	int sockfd, con_fd, ret;
 	struct sockaddr_in my_addr;
 	struct sockaddr_in their_addr;
 	int sin_size;
 
-//	char *buf;
 	pid_t pid;
 
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);          //create socket
-	if (sockfd <0)
-	{
+	//create socket
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (sockfd <0) {
 		printf("error in socket!");
 		exit(1);
 	}
 	
 	my_addr.sin_family = AF_INET;
 	my_addr.sin_port = htons(MYTCP_PORT);
-	my_addr.sin_addr.s_addr = htonl(INADDR_ANY);//inet_addr("172.0.0.1");
+	// inet_addr("172.0.0.1");
+	my_addr.sin_addr.s_addr = htonl(INADDR_ANY); 
 	bzero(&(my_addr.sin_zero), 8);
-	ret = bind(sockfd, (struct sockaddr *) &my_addr, sizeof(struct sockaddr));                //bind socket
-	if (ret <0)
-	{
+
+	// bind socket
+	ret = bind(sockfd, (struct sockaddr *) &my_addr, sizeof(struct sockaddr));
+
+	if (ret <0) {
 		printf("error in binding");
 		exit(1);
 	}
 	
-	ret = listen(sockfd, BACKLOG);                              //listen
+	// listen
+	ret = listen(sockfd, BACKLOG);
+
 	if (ret <0) {
 		printf("error in listening");
 		exit(1);
 	}
 
-	while (1)
-	{
+	while (1) {
 		printf("waiting for data\n");
 		sin_size = sizeof (struct sockaddr_in);
-		con_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);            //accept the packet
-		if (con_fd <0)
-		{
+
+		// accept the packet
+		con_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+		if (con_fd <0) {
 			printf("error in accept\n");
 			exit(1);
 		}
 
-		if ((pid = fork())==0)                                         // creat acception process
-		{
+		// create acception process
+		if ((pid = fork())==0) {
 			close(sockfd);
-			str_ser(con_fd);                                          //receive packet and response
+			// receive packet and response
+			str_ser(con_fd);
 			close(con_fd);
 			exit(0);
 		}
-		else close(con_fd);                                         //parent process
+		else {
+			// parent process
+			close(con_fd);
+		}
 	}
 	close(sockfd);
 	exit(0);
 }
 
-void str_ser(int sockfd)
-{
+void str_ser(int sockfd) {
 	char buf[BUFSIZE];
 	FILE *fp;
 	char recvs[DATALEN];
@@ -79,34 +87,39 @@ void str_ser(int sockfd)
 	
 	printf("receiving data!\n");
 
-	while(!end)
-	{
-		if ((n= recv(sockfd, &recvs, DATALEN, 0))==-1)                                   //receive the packet
-		{
+	while(!end) {
+		// receive the packet
+		if ((n= recv(sockfd, &recvs, DATALEN, 0))==-1) {
 			printf("error when receiving\n");
 			exit(1);
 		}
-		if (recvs[n-1] == '\0')									//if it is the end of the file
-		{
+
+		// if it is the end of the file
+		if (recvs[n-1] == '\0') {
 			end = 1;
 			n --;
 		}
+
 		memcpy((buf+lseek), recvs, n);
 		lseek += n;
 	}
+
 	ack.num = 1;
 	ack.len = 0;
-	if ((n = send(sockfd, &ack, 2, 0))==-1)
-	{
-			printf("send error!");								//send the ack
+
+	// send the ack
+	if ((n = send(sockfd, &ack, 2, 0))==-1) {
+			printf("send error!");
 			exit(1);
 	}
-	if ((fp = fopen ("myTCPreceive.txt","wt")) == NULL)
-	{
+
+	if ((fp = fopen ("myTCPreceive.txt","wt")) == NULL) {
 		printf("File doesn't exit\n");
 		exit(0);
 	}
-	fwrite (buf , 1 , lseek , fp);					//write data into file
+
+	// write data into file
+	fwrite (buf , 1 , lseek , fp);
 	fclose(fp);
 	printf("a file has been successfully received!\nthe total data received is %d bytes\n", (int)lseek);
 }
